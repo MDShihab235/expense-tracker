7689edd52c3c3d21fdfc741317ed93818a1bc5fd
MDShihab235 <msub111244@gmail.com>
2023-02-22 14:12:51 +0600

    first-commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
 import "./App.css";
+import Form from "./components/Form";
+import Graph from "./components/Graph";
 
 function App() {
   return (
     <div className="App">
-      <h1>Expence Tracker</h1>
+      <div className="container mx-auto max-w-6xl text-center drop-shadow-lg text-gray-800">
+        <h1 className="text-4xl py-8 mb-10 bg-slate-800 text-white rounded">
+          Expence Tracker
+        </h1>
+        {/* grid columns */}
+
+        <div className="grid md:grid-cols-2 gap-4">
+          {/* Chart */}
+          <Graph></Graph>
+          {/* Form */}
+          <Form></Form>
+        </div>
+      </div>
     </div>
   );
 }
